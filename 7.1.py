6749d8b995bc3c457fab0c43696a8282b4352764
shmelya008 <shmelya008@gmail.com>
2024-07-18 21:45:27 +0300

    Update 7.1.1.py

--- 7.1.py
+++ 7.1.py
@@ -9,7 +9,18 @@ class Product:
 
 
 class Shop:
-    __file_name = 'products.txt'
+    def __init__(self, __file_name):
+        self.__file_name = __file_name
+
+    def add(self, *products):
+        file = open(self.__file_name, 'a')
+        list_products = self.get_products()
+        for product in products:
+            if product.name and str(product.weight) not in list_products:
+                file.write(str(product) + '\n')
+            else:
+                print(f'Продукт {product.name} {product.weight} уже есть в магазине! (См. список: {self.__file_name})')
+        file.close()
 
     def get_products(self):
         file = open(self.__file_name, 'r')
@@ -17,23 +28,24 @@ class Shop:
         file.close()
         return products
 
-    def add(self, *products):
-        file = open(self.__file_name, 'a')
-        list_products = self.get_products()
-        for product in products:
-            if product.name not in list_products:
-                file.write(str(product) + '\n')
-            else:
-                print(f'Продукт {product.name} уже есть в магазине!')
-        file.close()
 
-
-s1 = Shop()
+s1 = Shop('products.txt')
+s2 = Shop('prod_2.txt')
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 p4 = Product('Apple', 20.3, 'Fruits')
+p5 = Product('Spaghetti', 3.4, 'Groceries')
+p6 = Product('Spaghetti', 3.6, 'Groceries')
 
-print(p2) # __str__
-s1.add(p1, p2, p3, p4)
+print(p2)  # __str__
+print()
+s1.add(p1, p2, p3)
 print(s1.get_products())
+s1.add(p5)
+s1.add(p6)
+print(s1.get_products())
+# s1.add(p4)
+# print(s1.get_products())
+# s2.add(p2, p4)
+# print(s2.get_products())
